a76e918412ad357096b9cbf568897bdf2c452497
Chris Sun <chris20090731@gmail.com>
2026-09-21 15:06:46 +0800

    update and test

--- quasar.config.js
+++ quasar.config.js
@@ -5,6 +5,44 @@ export default configure(() => ({
     warnings: true,
     errors: true
   },
+
+  boot: ['firebase', 'toast'],
+
+  css: ['app.scss'],
+
+  extras: ['material-icons'],
+
+  build: {
+    target: {
+      browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
+      node: 'node20'
+    },
+
+    vueRouterMode: 'history',
+
+    // GitHub Pages project sites are served from /Tickets/.
+    // Local development continues to use the root path.
+    publicPath: process.env.GITHUB_ACTIONS ? '/Tickets/' : '/',
+
+    distDir: 'dist/spa'
+  },
+
+  devServer: {
+    open: true
+  },
+
+  framework: {
+    config: {},
+    plugins: ['Notify']
+  }
+}))
+/*import { configure } from 'quasar/wrappers'
+
+export default configure(() => ({
+  eslint: {
+    warnings: true,
+    errors: true
+  },
   boot: ['firebase', 'toast'],
   css: ['app.scss'],
   extras: ['material-icons'],
