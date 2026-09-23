3ee841f01f453ec78177f2b1d8e941123c42c477
Chris Sun <chris20090731@gmail.com>
2026-09-23 14:03:29 +0800

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -20,9 +20,7 @@ export default configure(() => ({
 
     vueRouterMode: 'history',
 
-    // GitHub Pages project site is served from /Tickets/
-    // Local development uses the root path.
-    publicPath: process.env.GITHUB_ACTIONS ? '/Tickets/' : '/',
+    publicPath: '/',
 
     distDir: 'dist/spa'
   },
@@ -36,31 +34,3 @@ export default configure(() => ({
     plugins: ['Notify']
   }
 }))
-
-/*import { configure } from 'quasar/wrappers'
-
-export default configure(() => ({
-  eslint: {
-    warnings: true,
-    errors: true
-  },
-  boot: ['firebase', 'toast'],
-  css: ['app.scss'],
-  extras: ['material-icons'],
-  build: {
-    target: {
-      browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
-      node: 'node20'
-    },
-    vueRouterMode: 'history',
-    publicPath: '/',
-    distDir: 'dist/spa'
-  },
-  devServer: {
-    open: true
-  },
-  framework: {
-    config: {},
-    plugins: ['Notify']
-  }
-}))*/
