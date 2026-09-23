17e7843ff9ec15442f40d5a7b9a789d91c0f7dd9
Chris Sun <chris20090731@gmail.com>
2026-09-23 09:02:40 +0800

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -78,7 +78,7 @@ const routes = [
       },
       
       {
-        path: '/managemant',
+        path: 'managemant',
         name: 'management',
         component: () => import('pages/ManagementPage.vue'),
         meta: {
